08fd211a0dd4fea20ef12dc130c484268c3c69a7
suaib022 <suaib.109206@gmail.com>
2023-12-01 22:56:15 +0600

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://clean-co-service-2a44b.web.app',
         credentials: true,
     }
 ))
